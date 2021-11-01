e87d015f4e2eae3f4ba71de691ff106745aa6748
hurenren <hurenfa@gmail.com>
2021-11-02 00:24:28 +0800

    commit

--- 1.排序/3.快速排序.ts
+++ 1.排序/3.快速排序.ts
@@ -10,8 +10,38 @@
 // 适用场景：
 // 1. 任何场景
 
+function quicksort(list: number[], leftIndex: number, rightIndex: number) {
+    if (leftIndex > rightIndex) return; // 大的必须在右边，因为是从下一个开始对比，会出现一个大的永远在左边，所以 return
+
+    // 基准数
+    const temp = list[leftIndex];
+
+    // 运动坐标
+    let i = leftIndex;
+    let j = rightIndex;
+
+    while (i != j) {
+        // 从右往左找，小于 temp 的数，否则一直移动坐标
+        while (list[j] >= temp && i < j) j--;
+
+        // 从左往右找，大于 temp 的数，否则一直移动坐标
+        while (list[i] <= temp && i < j) i++;
+
+        // 说明两个坐标没有相遇，交换位置
+        if (i < j) [list[i], list[j]] = [list[j], list[i]];
+    }
+
+    // 将基准数与移动到的坐标对换位置，因为 i j 相遇的坐标数必定小于原基准数，因为 i 就是小于基准数的 j 对换过来的
+    list[leftIndex] = list[i];
+    list[i] = temp;
+
+    // 继续递归左半区与右半区
+    quicksort(list, leftIndex, i - 1);
+    quicksort(list, i + 1, rightIndex);
+
+    return list;
+}
+
 const numList: number[] = [6, 1, 2, 7, 9, 3, 4, 5, 10, 8];
 
-function quicksort() {
-
-}
+console.log(quicksort(numList, 0, numList.length - 1));
